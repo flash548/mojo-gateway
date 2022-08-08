fc84328b17d639a94d3873e53e3943692819453b
Chris Zell <christopher.zell@mail.com>
2022-08-08 05:59:56 -0400

    no yancy - lighter weight

--- gateway.pl
+++ gateway.pl
@@ -5,9 +5,9 @@ use Mojo::SQLite;
 use Mojo::Pg;
 use Mojo::JWT;
 use Time::Piece;
-use Date::Parse;
-
-use lib '.';
+use Crypt::Bcrypt qw/bcrypt bcrypt_check/;
+use Data::Entropy::Algorithms qw(rand_bits);
+use lib qw(./lib);
 use Password::Complexity;
 
 my $config = plugin 'JSONConfig';
@@ -19,7 +19,7 @@ helper db_conn => sub {
 
     # config var 'test' will be defined if we're running unit tests,
     # otherwise see what type of DB we defined in the ENV VARs
-    if ( !defined( $config->{test} ) && $config->{db_type} eq 'sqlite' ) {
+    if ( !$ENV{test} && $config->{db_type} eq 'sqlite' ) {
         state $path      = app->home->child('data.db');
         state $db_handle = Mojo::SQLite->new( 'sqlite:' . $path );
         return $db_handle;
@@ -35,79 +35,16 @@ helper db_conn => sub {
 
 # do migrations (see bottom of file __DATA__ section)
 app->db_conn->auto_migrate(1)->migrations->from_data;
-
-# initialize Yancy, editor enabled (requiring admin field True)
-plugin Yancy => {
-    backend => $config->{db_type} eq 'pg'
-    ? { Pg     => app->db_conn }
-    : { Sqlite => app->db_conn },
-    editor => {
-        require_user => { is_admin => 1 },
-    },
-    read_schema => 0,
-    schema      => {
-        users => {
-            'x-id-field' => 'email',
-            properties   => {
-                email => {
-                    type   => [ 'string', 'null' ],
-                    format => 'email',
-                },
-                dod_id => {
-                    type   => [ 'integer', 'null' ],
-                    format => 'number',
-                },
-                password => {
-                    type   => [ 'string', 'null' ],
-                    format => 'password',
-                },
-                is_admin => {
-                    type    => 'boolean',
-                    default => 0,
-                },
-                reset_password => {
-                    type    => 'boolean',
-                    default => 0,
-                },
-                last_reset => {
-                    type    => [ 'string' ],
-                    format  => 'date-time',
-                    default => 'now',  # default to now for when new user accounts made
-                }
-            },
-        },
-    },
-};
-
-# load the Yancy authN plugin, uses the 'users' table and email/password
-# as the username/password fields.  New user registration disabled.
-app->yancy->plugin(
-    'Auth' => {
-        schema         => 'users',
-        allow_register => 0,
-        plugins        => [
-            [
-                Password => {
-                    username_field  => 'email',
-                    password_field  => 'password',
-                    password_digest => {
-                        type => 'SHA-1',
-                    },
-                }
-            ],
-        ]
-    }
-);
-
+say $config->{admin_user};
 # create the admin user if it doesn't exist
-app->yancy->create(
+app->db_conn->db->insert(
     users => {
         email    => "$config->{admin_user}",
-        password => "$config->{admin_pass}",
+        password => encode_password($config->{admin_pass}),
         dod_id   => 123456789,
         is_admin => 1
     }
-) unless app->yancy->get( 'users', "$config->{admin_user}" )->{email};
+) unless defined(app->db_conn->db->select( 'users', undef, { email => $config->{admin_user}} )->hash);
 
 # proxy method - takes the request object ($c) and the URL ($uri)
 #   makes sure the $uri has no backslash on it
@@ -161,6 +98,41 @@ sub proxy ( $c, $name, $uri ) {
     }
 }
 
+sub check_pass($db_password, $password) {
+    return bcrypt_check($password, $db_password);
+}
+
+sub do_login($c) {
+    my $username = $c->req->param('username');
+    my $password = $c->req->param('password');
+
+    my $record = get_user($username);
+    if ($record && check_pass($record->{password}, $password)) {
+        delete $record->{password};
+
+        # fix an undef 'last_reset' to now
+        if ($record->{last_reset} == undef) {
+            app->db_conn->db->update("users", { last_reset => get_gmstamp() }, { email => $username });
+            $record = get_user($username);
+            delete $record->{password};
+        }
+            
+        $c->session->{user} = $record;
+        $c->redirect_to($c->flash('return_to') // '/');
+    } else {
+        $c->render( 'login_page', login_failed => 1 );
+    }
+}
+
+sub get_user($username) {
+    my $record = app->db_conn->db->select("users", undef, { email => $username })->hash;
+    return $record if defined($record);
+}
+
+sub encode_password($password) {
+    return bcrypt($password, "2b", 12, rand_bits(16*8));
+}
+
 # password must be at least 8 characters
 # must have letters, numbers, and at least one special character
 sub do_password_change($c) {
@@ -169,34 +141,51 @@ sub do_password_change($c) {
     my $retyped_password = $c->req->param('retyped-new-password');
     my $checker = Password::Complexity->new;
 
-    my ($obj) = app->yancy->auth->plugins;
-    if (!$existing_password || !$obj->_check_pass( $c, $c->yancy->auth->current_user->{email}, $existing_password )) {
+    if (!$existing_password || !check_pass( get_user($c->session->{user}->{email})->{password}, $existing_password )) {
         $c->flash({ return_to => $c->flash('return_to'), error_msg => 'Existing Password Incorrect'});
-        $c->redirect_to('/yancy/auth/password/change',  );
+        $c->redirect_to('/auth/password/change',  );
     } elsif ($new_password eq $existing_password) {
         $c->flash({ return_to => $c->flash('return_to'), error_msg => 'New Password cannot equal the existing password' });
-        $c->redirect_to('/yancy/auth/password/change',  );
+        $c->redirect_to('/auth/password/change',  );
     } elsif ($new_password ne $retyped_password) {
         $c->flash({ return_to => $c->flash('return_to'), error_msg => 'New Password does not equal Retyped New Password' });
-        $c->redirect_to('/yancy/auth/password/change',  );
+        $c->redirect_to('/auth/password/change',  );
     } elsif (!$checker->check_complexity(new_password => $new_password, $config->{password_complexity} )) {
         $c->flash({ return_to => $c->flash('return_to'), error_msg => 'New Password does not meet complexity requirements... No whitespace, at least 8 characters, combination of letters/digits and special characters.' });
-        $c->redirect_to('/yancy/auth/password/change', );
+        $c->redirect_to('/auth/password/change', );
     } else {
         # guess we're good
-        app->yancy->set("users", $c->yancy->auth->current_user->{email}, { "password" => $new_password, reset_password => 0, last_reset => 'now' } );
+        app->db_conn->db->update("users", { "password" => encode_password($new_password), reset_password => 0, last_reset => get_gmstamp() }, $c->session->{user}->{email} );
         $c->redirect_to($c->flash('return_to'));
     }
+}
+
+# returns 'now' as a ISO8601 UTC string (with no 'Z')
+sub get_gmstamp() {
+    return gmtime()->datetime;
+}
+
+# returns time since given date stamp (in ISO 8601 format) in days
+sub get_days_since_gmstamp($time) {
+    return (gmtime() - Time::Piece->strptime($time, "%Y-%m-%dT%H:%M:%S"))->days;
 }
 
 ############################
 #### BEGIN ROUTING DEFS ####
 ############################
 
-# logout shortcut - always reachable
+# login/logout shortcut - always reachable
 get '/logout' => sub ($c) {
     $c->session( expires => 1 );
-    $c->redirect_to('yancy.auth.password.login');
+    $c->redirect_to('/login');
+};
+
+get '/login' => sub ($c) {
+    $c->render('login_page');
+};
+
+post '/auth/login' => sub ($c) {
+    do_login($c);
 };
 
 # all routes from here-on require authentication
@@ -204,48 +193,46 @@ under '/' => sub ($c) {
 
     # if there's no current_user populated in the session cookie,
     # then we're not authenticated, so re-direct to the sign-in page...
-    unless ( $c->yancy->auth->current_user ) {
+    unless ( $c->session->{user}->{email} ) {
 
         # set return_to value to go back to initially requested url
         # dont allow redirect back to itself - default to /
-        if ( $c->req->url =~ m/auth\/password/ || !defined( $c->req->url ) ) {
-            $c->req->url = '/';
+        if ( $c->req->url =~ m/auth\/login/ || !defined( $c->req->url ) ) {
+            $c->req->url('/');
         }
         $c->flash( { return_to => $c->req->url } );
-        $c->redirect_to('yancy.auth.password.login');
+        $c->redirect_to('/login');
         return undef;
     }
 
     # if user record shows we need to reset-password, and we're not already at the password path,
     #  then re-direct to that page
-    if ( $c->yancy->auth->current_user->{reset_password} && $c->req->url !~ /\/yancy\/auth\/password\/change/ ) {
+    if ( $c->session->{user}->{reset_password} && $c->req->url !~ /\/auth\/password\/change/ ) {
         $c->flash( { return_to => $c->req->url } );
-        $c->redirect_to('/yancy/auth/password/change');
+        $c->redirect_to('/auth/password/change');
         return undef;
     }
 
     # user authenticated OK or already-authenticated, check password expiry if we've defined
     #  that setting and its more than 0 days
-    if ($c->yancy->auth->current_user->{reset_password} || ($config->{password_valid_days} && $config->{password_valid_days} > 0)
-        && $c->req->url !~ /\/yancy\/auth\/password\/change/) {
+    if ($c->session->{reset_password} || ($config->{password_valid_days} && $config->{password_valid_days} > 0)
+        && $c->req->url !~ /\/auth\/password\/change/) {
 
         # because we said so
-        if ($c->yancy->auth->current_user->{reset_password}) {
+        if ($c->session->{user}->{reset_password}) {
             $c->flash( { return_to => $c->req->url, expired => 0, mandated => 1 });
-            $c->redirect_to('/yancy/auth/password/change');
+            $c->redirect_to('/auth/password/change');
         }
 
         # if last_reset is undef, then set it to now, not sure how'd that be though, should default to current date time
-        if (!defined($c->yancy->auth->current_user->{last_reset})) {
-            app->yancy->set("users", $c->yancy->auth->current_user->{email}, { last_reset => 'now' } );
+        if ($c->session->{user}->{last_reset} == undef) {
+            app->db_conn->db->update("users", { last_reset => get_gmstamp() }, { email => $c->session->{user}->{email}} );
         } else {
             # check days delta between NOW and last_reset
-            my $last_reset = str2time($c->yancy->auth->current_user->{last_reset});
-            my $reset_timestamp = Time::Piece->new($last_reset);
-            my $now = Time::Piece->new();
-            if (Time::Seconds->new($now - $reset_timestamp)->days >= $config->{password_valid_days}) {
+            my $last_reset = $c->session->{user}->{last_reset};
+            if (get_days_since_gmstamp($last_reset) >= $config->{password_valid_days}) {
                 $c->flash( { return_to => $c->req->url, expired => 1, mandated => 0 });
-                $c->redirect_to('/yancy/auth/password/change');
+                $c->redirect_to('/auth/password/change');
                 return undef;
             }
         }
@@ -256,15 +243,15 @@ under '/' => sub ($c) {
 };
 
 # show the password change form
-get '/yancy/auth/password/change' => sub ($c) {
+get '/auth/password/change' => sub ($c) {
     # preserve the flash value
     $c->flash( { return_to => $c->flash('return_to') // '/' });
     # render the change_password form
-    $c->render('yancy/auth/password/password_change');
+    $c->render('password_change');
 };
 
 # change the password for current user
-post '/yancy/auth/password/change' => sub ($c) {    
+post '/auth/password/change' => sub ($c) {    
     do_password_change($c);
 };
 
